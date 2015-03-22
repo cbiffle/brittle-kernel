a61c556540385d7c7ca80dc8200b80c8eb000a67
Cliff L. Biffle <code@cliffle.com>
2015-03-22 09:28:31 -0700

    Bring over GDB support files from Rehash.

---
+++ gdb.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Intended use:
+#   $ gdb.sh build/latest/demo/demo
+# Note that you must have started openocd separately for this to work.
+# Once inside gdb, the gdbconfig gives you a 'flash' command.
+
+ROOT="$(dirname "$0")"
+
+arm-none-eabi-gdb -x "$ROOT/gdbconfig" $1
