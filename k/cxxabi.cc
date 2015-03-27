83dfbf5c916c487abc352c8f2869cebdea8cb24c
Cliff L. Biffle <code@cliffle.com>
2015-03-27 08:39:07 -0700

    k/cxxabi: add memset for ctors.

--- k/cxxabi.cc
+++ k/cxxabi.cc
@@ -1,5 +1,15 @@
 #include <cxxabi.h>
+#include <cstdint>
+#include <cstring>
 
 void __cxxabiv1::__cxa_pure_virtual() {
   while (true);
+}
+
+void * memset(void * s, int c, size_t n) {
+  auto s8 = static_cast<uint8_t *>(s);
+  for (size_t i = 0; i < n; ++i) {
+    s8[i] = uint8_t(c);
+  }
+  return s;
 }
