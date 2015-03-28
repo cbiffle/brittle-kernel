0f3c7c06dbd38c316d6f83f39f08ad0b0872c621
Cliff L. Biffle <code@cliffle.com>
2015-03-28 14:55:32 -0700

    k/list: more operations.

--- k/list.h
+++ k/list.h
@@ -1,6 +1,8 @@
 #ifndef K_LIST_H
 #define K_LIST_H
 
+#include "etl/assert.h"
+
 #include "k/config.h"
 
 namespace k {
@@ -25,6 +27,13 @@ public:
     T * owner;
 
     Item(T * o) : owner{o} {}
+
+    void unlink() {
+      this->next->prev = this->prev;
+      this->prev->next = this->next;
+
+      this->next = this->prev = this;
+    }
   };
 
   /*
@@ -35,6 +44,44 @@ public:
       _roots[i] = { &_roots[i], &_roots[i] };
     }
   }
+
+  /*
+   * Checks whether the list is currently empty.
+   *
+   * Time: linear in n_priorities.
+   */
+  bool is_empty() const {
+    for (auto const & it : _roots) {
+      if (it.next != &it) return false;
+    }
+    return true;
+  }
+
+  /*
+   * Unlinks the highest priority item from the list and returns its
+   * owner.
+   *
+   * Precondition: list is not empty.
+   *
+   * TODO: merging this with the empty check and returning a Maybe would
+   * improve performance.
+   *
+   * Time: linear in n_priorities.
+   */
+  T * take() {
+    for (auto const & r : _roots) {
+      if (r.next != &r) {
+        // Cast safe due to invariant that sublists only contain a single
+        // Itemoid, and it is the root.
+        auto it = static_cast<Item *>(r.next);
+        it->unlink();
+        return it->owner;
+      }
+    }
+
+    ETL_ASSERT(false);
+  }
+
 
 private:
   Itemoid _roots[config::n_priorities];
