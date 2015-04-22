4f4aa60a658070555bebdd506c01cc69465a959b
Cliff L. Biffle <code@cliffle.com>
2015-04-22 09:25:35 -0700

    demo: add an idle task.
    This "idle task" is a placeholder that should not get any CPU cycles in
    the current configuration -- if it does, it will assert.  (Which won't
    actually take the system down, but will be detectable in GDB.)

--- demo/demo.cc
+++ demo/demo.cc
@@ -1,5 +1,7 @@
 #include <cstdint>
 
+#include "etl/assert.h"
+
 #include "demo/client/client.h"
 #include "demo/driver/driver.h"
 #include "demo/k/context.h"
@@ -15,7 +17,8 @@ static constexpr unsigned
   oi_peripheral_region = 4,
   oi_gate = 5,
   oi_initial_context = 6,
-  oi_second_context = 7;
+  oi_second_context = 7,
+  oi_idle_context = 8;
 
 // Our use of key registers:
 static constexpr unsigned
@@ -23,7 +26,8 @@ static constexpr unsigned
   k_periph = 5,
   k_self = 6,
   k_second = 7,
-  k_gate = 8;
+  k_idle = 8,
+  k_gate = 9;
 
 static void collect_keys() {
   // Awkwardly produce a key to the peripheral address space region.
@@ -33,14 +37,71 @@ static void collect_keys() {
                          k_periph);
   // Slightly less awkwardly produce our own context key.
   object_table::mint_key(k_object_table, oi_initial_context, 0, k_self);
-  // Get a key to the second context.
+  // Get a key to the other contexts.
   object_table::mint_key(k_object_table, oi_second_context, 0, k_second);
+  object_table::mint_key(k_object_table, oi_idle_context, 0, k_idle);
   // aaaand the IPC gate.
   object_table::mint_key(k_object_table, oi_gate, 0, k_gate);
 }
 
 static void enable_peripheral_access() {
   context::set_region(k_self, 2, k_periph);
+}
+
+static uint32_t idle_stack[128];
+
+static void idle_main() {
+  while (true) {
+    ETL_ASSERT(false);
+  }
+}
+
+static void spawn_idle() {
+  // Inflate the nascent Context into a little task.
+
+  // Load its memory region keys so it can actually run.  For now, we'll just
+  // share our memory authority.  TODO: it could be limited further.
+  for (unsigned i = 0; i < 6; ++i) {
+    context::get_region(k_self, i, 1);
+    context::set_region(k_idle, i, 1);
+  }
+
+  // Give it a valid stack.  We need to do this before setting machine
+  // registers, as some of them live in the exception frame at the moment.
+  auto idle_stack_frame = &idle_stack[128 - 8];
+  context::set_register(k_idle, 13,
+      reinterpret_cast<uint32_t>(idle_stack_frame));
+
+  // Now, set the machine registers.
+  for (unsigned i = 0; i < 17; ++i) {
+    uint32_t register_value;
+    switch (i) {
+      case 13:
+        continue;  // handled above.
+
+      case 15:
+        register_value = reinterpret_cast<uint32_t>(idle_main);
+        break;
+
+      case 16:
+        register_value = 1 << 24;  // set thumb mode
+        break;
+
+      default:
+        register_value = 0xDEADBEEF;
+        break;
+    }
+
+    context::set_register(k_idle, i, register_value);
+  }
+
+  // Provide it with the gate key, so it can do something.
+  context::set_key(k_idle, 4, k_gate);
+
+  // Set up its execution parameters and let it run.
+  // It won't run yet, of course; we haven't yielded control.
+  context::set_priority(k_idle, 1);
+  context::make_runnable(k_idle);
 }
 
 static uint32_t client_stack[128];
@@ -95,7 +156,8 @@ static void spawn_client() {
 void main() {
   // Derive our authority from the kernel's representation.
   collect_keys();
-  // Fire up the client.
+  // Fire up the other tasks.
+  spawn_idle();
   spawn_client();
 
   // Now, become a UART driver.  Grant access to the APB.
--- k/config.h
+++ k/config.h
@@ -5,13 +5,13 @@ namespace k {
 namespace config {
 
 static constexpr unsigned
-  n_contexts = 2,
-  n_objects = 10,
+  n_contexts = 3,
+  n_objects = 12,
   n_task_keys = 16,
   n_task_regions = 6,
   n_message_data = 4,
   n_message_keys = 4,
-  n_priorities = 4;
+  n_priorities = 2;
 
 }  // namespace config
 }  // namespace k
