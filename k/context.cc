e709aaa924b217fb391fb4b6783027ebeb256108
Cliff L. Biffle <code@cliffle.com>
2015-04-28 14:15:56 -0700

    k/context: remove duplicate ctx_item unlinking.
    In practice, the caller has already unhooked the context from whatever
    had blocked it.  Doing it twice just hurt performance.

--- k/context.cc
+++ k/context.cc
@@ -116,7 +116,6 @@ void Context::block_in_receive(List<Context> & list) {
 }
 
 void Context::complete_blocked_receive(Brand brand, Sender * sender) {
-  _ctx_item.unlink();  // from the receiver list
   runnable.insert(&_ctx_item);
   _state = State::runnable;
 
