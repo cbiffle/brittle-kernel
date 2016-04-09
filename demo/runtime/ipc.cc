5c69262491a9f62bfdfe5bb6a0c09065f31b7988
Cliff L. Biffle <code@cliffle.com>
2016-04-09 12:14:55 -0700

    demo/runtime/ipc: small performance improvements.
    Copy Key turns out to be an excellent candidate for inlining, since it
    changes no registers.
    
    The IPC operation was saving/restoring two registers more than strictly
    necessary in the current ABI.

--- demo/runtime/ipc.cc
+++ demo/runtime/ipc.cc
@@ -3,22 +3,10 @@
 __attribute__((naked))
 ReceivedMessage ipc(Message const &) {
   asm volatile (
-      "push {r4-r11, lr}\n"
+      "push {r4-r9, lr}\n"
       "ldm r1, {r4, r5, r6, r7}\n"
       "svc #0\n"
       "stm r0, {r4, r5, r6, r7, r8, r9}\n"
-      "pop {r4-r11, pc}\n"
-      );
-}
-
-__attribute__((naked))
-void copy_key(unsigned to, unsigned from) {
-  asm volatile(
-      "push {r4, lr}\n"
-      "lsls r4, r0, #20\n"          // Shift "to" into target field
-      "orrs r4, r4, r1, lsl #24\n"  // Combine "from" in source field
-      "orrs r4, r4, #(1 << 28)\n"   // Add sysnum
-      "svc #0\n"
-      "pop {r4, pc}\n"
+      "pop {r4-r9, pc}\n"
       );
 }
--- demo/runtime/ipc.h
+++ demo/runtime/ipc.h
@@ -3,10 +3,26 @@
 
 #include <cstdint>
 
+#include "etl/attribute_macros.h"
+#include "common/descriptor.h"
 #include "common/message.h"
 
 ReceivedMessage ipc(Message const &);
 
-void copy_key(unsigned to, unsigned from);
+ETL_INLINE
+void copy_key(unsigned to, unsigned from) {
+  auto d = Descriptor::zero()
+    .with_sysnum(1)
+    .with_source(from)
+    .with_target(to);
+
+  register auto d_bits asm ("r4") = uint32_t(d);
+
+  asm volatile(
+      "svc #0"
+      : /* no outputs */
+      : "r"(d_bits)
+      );
+}
 
 #endif  // DEMO_RUNTIME_IPC_H
