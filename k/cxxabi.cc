9b16a00c33a01ffd46100ccd853d6523e81e94a8
Cliff L. Biffle <code@cliffle.com>
2016-04-11 09:14:36 -0700

    k/cxxabi: improve memset throughput.

--- k/cxxabi.cc
+++ k/cxxabi.cc
@@ -9,24 +9,24 @@ void __cxxabiv1::__cxa_pure_virtual() {
 void * memset(void * s, int c, size_t n) {
   // Write out enough single bytes to align the output pointer.
   auto p8 = static_cast<uint8_t *>(s);
-  while (n && (reinterpret_cast<uintptr_t>(p8) & 3)) {
+  auto end = static_cast<uint8_t *>(s) + n;
+  while (p8 != end && (reinterpret_cast<uintptr_t>(p8) & 3)) {
     *p8++ = uint8_t(c);
-    --n;
   }
 
   // Double-cast to dodge alignment check (we know it's aligned)
   auto p32 = static_cast<uint32_t *>(static_cast<void *>(p8));
+  auto end32 = reinterpret_cast<uint32_t *>(
+      reinterpret_cast<uintptr_t>(end) & ~3);
   // Begin writing word-sized chunks.
-  while (n >= 4) {
+  while (p32 != end32) {
     *p32++ = uint32_t(c) * 0x01010101;
-    n -= 4;
   }
 
   // Finish up using single bytes.
-  p8 = reinterpret_cast<uint8_t *>(p32);
-  while (n) {
+  p8 = reinterpret_cast<uint8_t *>(end32);
+  while (p8 != end) {
     *p8++ = uint8_t(c);
-    --n;
   }
 
   return s;
