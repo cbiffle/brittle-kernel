82bb08b36301fbfc8ea9f43bd8394e84cb73401b
Cliff L. Biffle <code@cliffle.com>
2016-04-20 17:13:46 -0700

    demo/k/memory: expose memory API

---
+++ demo/k/memory.cc
@@ -0,0 +1,50 @@
+#include "demo/k/memory.h"
+
+#include "etl/assert.h"
+
+#include "demo/runtime/ipc.h"
+
+namespace memory {
+
+void split(unsigned k, unsigned slot_key,
+    unsigned top_key_out) {
+  copy_key(1, slot_key);
+
+  auto rm = ipc({Descriptor::call(2, k)});
+  ETL_ASSERT(!rm.m.d0.get_error());
+
+  copy_key(k, 1);
+  copy_key(top_key_out, 2);
+}
+
+void become(unsigned k, ObjectType ot, unsigned arg) {
+  auto rm = ipc({
+      Descriptor::call(3, k),
+      uint32_t(ot),
+      arg,
+      });
+  ETL_ASSERT(!rm.m.d0.get_error());
+
+  copy_key(k, 1);
+}
+
+uint32_t peek(unsigned k, uint32_t offset) {
+  auto rm = ipc({
+      Descriptor::call(4, k),
+      offset,
+      });
+  ETL_ASSERT(!rm.m.d0.get_error());
+
+  return rm.m.d1;
+}
+
+void poke(unsigned k, uint32_t offset, uint32_t word) {
+  auto rm = ipc({
+      Descriptor::call(5, k),
+      offset,
+      word,
+      });
+  ETL_ASSERT(!rm.m.d0.get_error());
+}
+
+}  // namespace memory
---
+++ demo/k/memory.h
@@ -0,0 +1,25 @@
+#ifndef DEMO_MEMORY_H
+#define DEMO_MEMORY_H
+
+#include <cstdint>
+
+namespace memory {
+
+void split(unsigned k, unsigned slot_key, unsigned top_key_out);
+
+enum class ObjectType : uint32_t {
+  context = 0,
+  gate,
+  reply_gate,
+  interrupt,
+  sys_tick,
+};
+
+void become(unsigned k, ObjectType, unsigned arg);
+
+uint32_t peek(unsigned k, uint32_t offset);
+void poke(unsigned k, uint32_t offset, uint32_t data);
+
+}  // namespace memory
+
+#endif  // DEMO_MEMORY_H
